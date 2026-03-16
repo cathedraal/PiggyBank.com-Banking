c55206fc69dd512680770a84d568ad048146ae7d
cathedraal <whiteobamama@gmail.com>
2026-03-16 19:05:26 +0100

    added JSDoc to articlePage.js

--- interfaces/articlePage.js
+++ interfaces/articlePage.js
@@ -1,21 +1,31 @@
+/**
+ * @overview Page where user can read an article.
+ * @author Roman Mallindine
+ */
+
 import { renderHeader, renderFooter, renderNewsPage, renderBackButton } from "../logics/rendering.js";
 import { wrapper } from "../logics/globals.js";
 
 
 export function articlePage(value) {
     wrapper.innerHTML = ''
+
+    // header
     renderHeader(wrapper, "article", "Enjoying this article?")
 
     const newsPageContainer = document.createElement('section')
     newsPageContainer.classList.add('newsPage-container')
     wrapper.append(newsPageContainer)
 
+    // back button
     renderBackButton(newsPageContainer)
 
+    // article
     renderNewsPage(newsPageContainer, value)
 
+    // footer
     renderFooter(wrapper)
 
-    // scroll when rendered
+    // scroll up when rendered
     wrapper.scrollTo({ top: 0, behavior: 'smooth' })
 }
