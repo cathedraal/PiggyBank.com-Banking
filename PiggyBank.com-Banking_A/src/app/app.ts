78a6b1ebd131c2b867070880e47155d31645e5e0
cathedraal <whiteobamama@gmail.com>
2026-04-10 17:53:02 +0200

    Feat: Add smooth scroll via Lenis library in AppComponent

--- PiggyBank.com-Banking_A/src/app/app.ts
+++ PiggyBank.com-Banking_A/src/app/app.ts
@@ -1,14 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
 import { HeaderComponent } from './components/header/header';
+import Lenis from 'lenis';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Footer, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('PiggyBank.com-Banking_A');
+
+  ngOnInit(): void {
+    const lenis = new Lenis({ lerp: 0.08 });
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+  }
 }
