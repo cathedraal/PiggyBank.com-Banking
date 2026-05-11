fc64ed7a9b14d8b155034d1b04cfeaf145da5784
RomanSuse <whiteobamama@gmail.com>
2026-05-11 14:09:50 +0200

    Refactor (API): Replace fetch().then() with RxJS pipeline

--- src/app/services/currency-api/currency-api.ts
+++ src/app/services/currency-api/currency-api.ts
@@ -1,20 +1,58 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable, signal } from '@angular/core';
+import { Observable, Subject, takeUntil } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CurrencyApiService {
-  private url: string = 'https://api.frankfurter.dev/v2/rates?quotes=USD,GBP';
+  private readonly url: string = 'https://api.frankfurter.dev/v2/rates?quotes=USD,GBP';
   public usd = signal<number>(0);
   public gbp = signal<number>(0);
+  private unsubscribe = new Subject<void>();
 
-  loadRates(): void {
+  // DI
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Fetch API, no rxjs, loads rates -------> not in use
+   */
+  loadRatesFetch(): void {
     fetch(this.url)
-      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.status);
+        return response.json();
+      })
       .then((data: Array<any>) => {
         this.usd.set(data.find((item) => item.quote === 'USD').rate);
         this.gbp.set(data.find((item) => item.quote === 'GBP').rate);
       })
       .catch((err) => console.log(err));
+  }
+
+  /**
+   * API with RxJS, loads rates -------> not in use
+   */
+  loadRatesRxjs(): void {
+    this.http
+      .get<Array<any>>(this.url)
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe({
+        next: (data) => {
+          this.usd.set(data.find((item) => item.quote === 'USD').rate);
+          this.gbp.set(data.find((item) => item.quote === 'GBP').rate);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
+
+  /**
+   * Gets rates from the URL
+   * @returns Array of rates
+   */
+  getRates(): Observable<Array<any>> {
+    return this.http.get<Array<any>>(this.url);
   }
 }
